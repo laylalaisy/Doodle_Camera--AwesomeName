4e05127feb7456feab7d05df9b11963468ad3ad7
laylalaisy <lsylayla@163.com>
2019-01-15 04:41:51 -0800

    update csv2jpg

--- similarity/csv2jpg.py
+++ similarity/csv2jpg.py
@@ -1,12 +1,2 @@
-from PIL import Image, ImageDraw
-import csv
-
-out = Image.new("L",(49,87))
-dout = ImageDraw.Draw(out)
-import csv
-with open('pic.txt', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        dout.point((int(row[0]) / 10,int(row[1]) / 10),fill=int(int(row[2]) * 2.55))
-        #print(row[0] + " " + row[1] + " " + row[2])
-out.show()
+from numpy import genfromtxt
+my_data = genfromtxt('csv.csv', delimiter=',')
