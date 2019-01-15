e24c49900ecc81eae464634f46bd833c20dc6b21
laylalaisy <lsylayla@163.com>
2019-01-15 07:06:48 -0800

    update edge detection

--- similarity/edge_detection.py
+++ similarity/edge_detection.py
@@ -2,15 +2,19 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-img = cv2.imread('dataset_path/cat/0.png',0)
-edges = cv2.Canny(img,100,200) #参数:图片，minval，maxval,kernel = 3
+# input original image
+img_ori = cv2.imread('./dataset/photos/image/dog/0.jpg',0)
 
-plt.subplot(121) #121表示行数，列数，图片的序号即共一行两列，第一张图
-plt.imshow(img,cmap='gray') #cmap :colormap 设置颜色
-plt.title('original image'),plt.xticks([]),plt.yticks([]) #坐标轴起点，终点的值
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('edge image'),plt.xticks([]),plt.yticks([])
+# do edge detection
+img_edges = cv2.Canny(img_ori,100,200)
 
+# resize image: 256*256
+img_resize = cv2.resize(img_edges, (256,256), interpolation=cv2.INTER_CUBIC)
+
+# save 
+cv2.imwrite('./dataset/photos/image/dog/0_edge.jpg', img_resize)
+
+print(img_resize)
+
+plt.imshow(img_resize,cmap = 'gray')
 plt.show()
-
-
