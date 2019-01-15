6f2d819171e3ec32f4dd9beaca3e672cdcd88473
laylalaisy <lsylayla@163.com>
2019-01-15 08:50:02 -0800

    simpl run

--- similarity/edge_detection.py
+++ similarity/edge_detection.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # input original image
-img_orig = cv2.imread('./dataset/doodles/image_orig/dog/0.png',0)
+img_orig = cv2.imread('./dataset/doodles/image_orig/dog/9.png',0)
 
 # do edge detection
-img_edges = cv2.Canny(img_orig,100,300)
+img_edges = cv2.Canny(img_orig,100,200)
 
 # resize image: 256*256
 img_resize = cv2.resize(img_edges, (256,256), interpolation=cv2.INTER_CUBIC)
@@ -15,7 +15,11 @@ img_resize = cv2.resize(img_edges, (256,256), interpolation=cv2.INTER_CUBIC)
 # cv2.imwrite('./dataset/doodles/image_edge/dog/0_edge.jpg', img_resize)
 
 # save
-np.savetxt('./dataset/doodles/edge_matrix/dog/0.csv', img_resize, delimiter = ',')
+for i in range(256):
+	for j in range(256):
+		if img_resize[i][j] != 0:
+			img_resize[i][j] = 1
+np.savetxt('./dataset/doodles/edge_matrix/dog/9.csv', img_resize, delimiter = ',')
 
 # plt.imshow(img_resize,cmap = 'gray')
 # plt.show()
