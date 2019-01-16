f86abc49bfef9112703d2986436471d7448bd28e
chenkaiyu1997 <justryit8@gmail.com>
2019-01-16 21:01:53 +0800

    asdf

--- shua/src/test_dense_finetune.py
+++ shua/src/test_dense_finetune.py
@@ -39,41 +39,41 @@ np.random.seed(seed=1987)
 tf.set_random_seed(seed=1987)
 
 def f2cat(filename: str) -> str:
-	return filename.split('.')[0]
+    return filename.split('.')[0]
 
 def list_all_categories():
-	files = os.listdir(os.path.join(INPUT_DIR, 'train_simplified'))
-	return sorted([f2cat(f) for f in files], key=str.lower)
+    files = os.listdir(os.path.join(INPUT_DIR, 'train_simplified'))
+    return sorted([f2cat(f) for f in files], key=str.lower)
 
 
 
 def apk(actual, predicted, k=3):
-	"""
-	Source: https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
-	"""
-	if len(predicted) > k:
-		predicted = predicted[:k]
-	score = 0.0
-	num_hits = 0.0
-	for i, p in enumerate(predicted):
-		if p in actual and p not in predicted[:i]:
-			num_hits += 1.0
-			score += num_hits / (i + 1.0)
-	if not actual:
-		return 0.0
-	return score / min(len(actual), k)
+    """
+    Source: https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
+    """
+    if len(predicted) > k:
+        predicted = predicted[:k]
+    score = 0.0
+    num_hits = 0.0
+    for i, p in enumerate(predicted):
+        if p in actual and p not in predicted[:i]:
+            num_hits += 1.0
+            score += num_hits / (i + 1.0)
+    if not actual:
+        return 0.0
+    return score / min(len(actual), k)
 
 def mapk(actual, predicted, k=3):
-	"""
-	Source: https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
-	"""
-	return np.mean([apk(a, p, k) for a, p in zip(actual, predicted)])
+    """
+    Source: https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/average_precision.py
+    """
+    return np.mean([apk(a, p, k) for a, p in zip(actual, predicted)])
 
 def preds2catids(predictions):
-	return pd.DataFrame(np.argsort(-predictions, axis=1)[:, :3], columns=['a', 'b', 'c'])
+    return pd.DataFrame(np.argsort(-predictions, axis=1)[:, :3], columns=['a', 'b', 'c'])
 
 def top_3_accuracy(y_true, y_pred):
-	return top_k_categorical_accuracy(y_true, y_pred, k=3)
+    return top_k_categorical_accuracy(y_true, y_pred, k=3)
 
 
 STEPS = 800
@@ -87,42 +87,42 @@ model = NASNetMobile(input_shape=(size, size, 1), weights=None, classes=NCATS)
 model.load_weights("model_dense121.h5")
 
 model.compile(optimizer=Adam(lr=0.002), loss='categorical_crossentropy',
-			  metrics=[categorical_crossentropy, categorical_accuracy, top_3_accuracy])
+              metrics=[categorical_crossentropy, categorical_accuracy, top_3_accuracy])
 print(model.summary())
 
 def draw_cv2(raw_strokes, size=256, lw=6, time_color=True):
-	img = np.zeros((BASE_SIZE, BASE_SIZE), np.uint8)
-	for t, stroke in enumerate(raw_strokes):
-		for i in range(len(stroke[0]) - 1):
-			color = 255 - min(t, 10) * 13 if time_color else 255
-			_ = cv2.line(img, (stroke[0][i], stroke[1][i]),
-						 (stroke[0][i + 1], stroke[1][i + 1]), color, lw)
-	if size != BASE_SIZE:
-		return cv2.resize(img, (size, size))
-	else:
-		return img
+    img = np.zeros((BASE_SIZE, BASE_SIZE), np.uint8)
+    for t, stroke in enumerate(raw_strokes):
+        for i in range(len(stroke[0]) - 1):
+            color = 255 - min(t, 10) * 13 if time_color else 255
+            _ = cv2.line(img, (stroke[0][i], stroke[1][i]),
+                         (stroke[0][i + 1], stroke[1][i + 1]), color, lw)
+    if size != BASE_SIZE:
+        return cv2.resize(img, (size, size))
+    else:
+        return img
 
 def image_generator_xd(size, batchsize, ks, lw=6, time_color=True):
-	while True:
-		for k in np.random.permutation(ks):
-			filename = os.path.join(DP_DIR, 'train_k{}.csv.gz'.format(k))
-			for df in pd.read_csv(filename, chunksize=batchsize):
-				df['drawing'] = df['drawing'].apply(json.loads)
-				x = np.zeros((len(df), size, size, 1))
-				for i, raw_strokes in enumerate(df.drawing.values):
-					x[i, :, :, 0] = draw_cv2(raw_strokes, size=size, lw=lw,
-											 time_color=time_color)
-				x = preprocess_input(x).astype(np.float32)
-				y = keras.utils.to_categorical(df.y, num_classes=NCATS)
-				yield x, y
+    while True:
+        for k in np.random.permutation(ks):
+            filename = os.path.join(DP_DIR, 'train_k{}.csv.gz'.format(k))
+            for df in pd.read_csv(filename, chunksize=batchsize):
+                df['drawing'] = df['drawing'].apply(json.loads)
+                x = np.zeros((len(df), size, size, 1))
+                for i, raw_strokes in enumerate(df.drawing.values):
+                    x[i, :, :, 0] = draw_cv2(raw_strokes, size=size, lw=lw,
+                                             time_color=time_color)
+                x = preprocess_input(x).astype(np.float32)
+                y = keras.utils.to_categorical(df.y, num_classes=NCATS)
+                yield x, y
 
 def df_to_image_array_xd(df, size, lw=6, time_color=True):
-	df['drawing'] = df['drawing'].apply(json.loads)
-	x = np.zeros((len(df), size, size, 1))
-	for i, raw_strokes in enumerate(df.drawing.values):
-		x[i, :, :, 0] = draw_cv2(raw_strokes, size=size, lw=lw, time_color=time_color)
-	x = preprocess_input(x).astype(np.float32)
-	return x
+    df['drawing'] = df['drawing'].apply(json.loads)
+    x = np.zeros((len(df), size, size, 1))
+    for i, raw_strokes in enumerate(df.drawing.values):
+        x[i, :, :, 0] = draw_cv2(raw_strokes, size=size, lw=lw, time_color=time_color)
+    x = preprocess_input(x).astype(np.float32)
+    return x
 
 
 valid_df = pd.read_csv(os.path.join(DP_DIR, 'train_k{}.csv.gz'.format(NCSVS - 1)), nrows=34000)
@@ -137,10 +137,10 @@ x, y = next(train_datagen)
 n = 8
 fig, axs = plt.subplots(nrows=n, ncols=n, sharex=True, sharey=True, figsize=(12, 12))
 for i in range(n**2):
-	ax = axs[i // n, i % n]
-	(-x[i]+1)/2
-	ax.imshow((-x[i, :, :, 0] + 1)/2, cmap=plt.cm.gray)
-	ax.axis('off')
+    ax = axs[i // n, i % n]
+    (-x[i]+1)/2
+    ax.imshow((-x[i, :, :, 0] + 1)/2, cmap=plt.cm.gray)
+    ax.axis('off')
 plt.tight_layout()
 fig.savefig('gs.png', dpi=300)
 # plt.show();
