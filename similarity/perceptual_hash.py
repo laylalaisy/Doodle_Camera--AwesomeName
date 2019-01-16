df859722210fbd3e48528350f5e5a01c7790cb4f
laylalaisy <lsylayla@163.com>
2019-01-16 01:11:35 -0800

    update match.xlsx

--- similarity/perceptual_hash.py
+++ similarity/perceptual_hash.py
@@ -1,14 +1,21 @@
+import sys
 from PIL import Image
 import imagehash
 
-hash = imagehash.average_hash(Image.open('0.jpg'))
-min = 10000000
-for i in range(999):
-    otherhash = imagehash.average_hash(Image.open('./pencil_img/'+str(i)+'.png'))
-    dist = hash - otherhash
-    if min > dist:
-        min = dist
-        idx = i
-print(min)
-print(idx)
+DOODLENUM = 1000
+
+if __name__ == "__main__":
+    label = sys.argv[1]
+    photo_filename = sys.argv[2]
+    doodle_foldername = sys.argv[3]
+    photo_hash = imagehash.average_hash(Image.open(photo_filename))
+    min_dist = float('inf')
+    for i in range(DOODLENUM):
+        doodle_hash = imagehash.average_hash(Image.open(doodle_foldername+str(i)+'.png'))
+        dist = photo_hash - doodle_hash
+        if min_dist > dist:
+            min_dist = dist
+            min_idx = i
+    print(min_dist)
+    print(min_idx)
 
