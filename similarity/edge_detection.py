bb4626a820a9c031cd66795ab4e51e128f9ef39b
laylalaisy <lsylayla@163.com>
2019-01-16 00:17:09 -0800

    add sift

--- similarity/edge_detection.py
+++ similarity/edge_detection.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
 
 	# input original image
-	img_orig = cv2.imread('./dataset/doodles/image_orig/dog/9.png',0)
+	img_orig = cv2.imread('0.jpg',0)
 
 	# do edge detection
 	img_edges = cv2.Canny(img_orig,100,200)
@@ -15,13 +15,15 @@ if __name__ == "__main__":
 
 	# save resize image
 	# cv2.imwrite('./dataset/doodles/image_edge/dog/0_edge.jpg', img_resize)
+	cv2.imwrite('./0_edge.jpg', img_resize)
 
 	# save
-	for i in range(256):
-		for j in range(256):
-			if img_resize[i][j] != 0:
-				img_resize[i][j] = 1
+	# for i in range(256):
+	# 	for j in range(256):
+	# 		if img_resize[i][j] != 0:
+	# 			img_resize[i][j] = 1
 	np.savetxt('./dataset/doodles/edge_matrix/dog/9.csv', img_resize, delimiter = ',')
 
-	# plt.imshow(img_resize,cmap = 'gray')
-	# plt.show()
+
+	plt.imshow(img_resize,cmap = 'gray')
+	plt.show()
